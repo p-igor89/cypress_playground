4f8ca5a41fd8aad8a2aab62ef848775783f64212
Peretiatko Ihor <p_ihor@hotmail.com>
2023-06-29 13:40:39 -0400

    add first part of the tests with contain logic

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
+module.exports = {
+  testingType: 'e2e',
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    excludeSpecPattern: [
+      '**/e2e/1-getting-started/*.cy.js',
+      '**/e2e/2-advanced-examples/*.cy.js',
+    ],
   },
-});
+};
---
+++ cypress/e2e/firstTest.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+it('Using get with Find and Eq', () => {
+    cy.viewport(1920, 1080)
+    cy.visit('next.privat24.ua/deposit/open')
+    cy.get('tbody').find('td').find('div').find('button').eq(0);
+})
+
+it('Works with contains option', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('Sign in'); //return button with text 'Sign in'
+})
+
+it('Works with contains option and two params', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('div', 'Sign in'); //return div with text 'Sign in'
+})
+
+it('Works with contains option and ignoring case', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('SIGN IN', {matchCase:false}); //return button, but ignore case
+})
